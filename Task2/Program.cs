5682fcd49e85e06dee9c8aa0b15738dad9b91b34
AnnaZimina <xadea@yandex.ru>
2022-08-06 11:44:43 +0300

    Task 2 find max between three numbers

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine("max = " + max);
