403258fcccd1ab99681d07b98edf396290d3b429
AnnaZimina <xadea@yandex.ru>
2022-08-06 12:21:36 +0300

    Task 4 find even numbers from 1 to N

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Let's find all even numbers from 1 to N. Input your N : ");
+double N = Convert.ToInt32(Console.ReadLine());
+
+double count = 1, rem = 1;
+
+Console.Write("From 1 to " + N + " even numbers are: ");
+
+while(count < N + 1)
+{
+    rem = count % 2;
+    if (rem == 0) Console.Write(count + ", ");
+    count = count + 1;
+}
