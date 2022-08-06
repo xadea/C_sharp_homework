613726de7c754405f23f8c27a35bbfe6f1d62840
AnnaZimina <xadea@yandex.ru>
2022-08-06 11:58:36 +0300

    Task 3 check if number ie even

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа, которая на вход принимает число и выдаёт, 
+// является ли число чётным
+
+Console.Write("Input number: ");
+double num = Convert.ToInt32(Console.ReadLine());
+
+double rem = num % 2;
+
+if (rem == 0) Console.WriteLine(num + " is an even number"); //простите, у меня терминал не воспринимает кириллицу, пишу на английском. even - переводится, как "четное"
+else Console.WriteLine(num + " is NOT an even number");
+
+ 
